e732cffbb6311f9e6f1f587eee0320f2cf733ef5
imhui <yjh19980105@gmail.com>
2021-08-05 23:18:45 +0800

    mark gateway filter

--- src/main/java/com/imhui/gateway/filter/AuthenticationFilter.java
+++ src/main/java/com/imhui/gateway/filter/AuthenticationFilter.java
@@ -1,9 +1,13 @@
 package com.imhui.gateway.filter;
 
+import com.imhui.gateway.common.ResponseResult;
+import com.imhui.gateway.common.ResponseUtil;
+import com.imhui.gateway.common.enums.ResponseCodeEnum;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -23,5 +27,17 @@ public class AuthenticationFilter implements GatewayFilter, Ordered {
     @Override
     public int getOrder() {
         return 0;
+    }
+
+    private String getAuthHeader(ServerHttpRequest request){
+        return request.getHeaders().getOrEmpty("Authorization").get(0);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, ResponseCodeEnum responseCode){
+        ServerHttpResponse response = exchange.getResponse();
+        ResponseResult result = ResponseUtil.fail(responseCode);
+
+//        return response.writeAndFlushWith(result);
+        return response.setComplete();
     }
 }
